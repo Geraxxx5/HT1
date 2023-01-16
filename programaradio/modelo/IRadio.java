09e197069e54665ba14ddf01db3c41537ed492bc
Geraxxx5 <gerax.pineda@gmail.com>
2023-01-16 10:39:57 -0600

    cambio de interfaz

---
+++ programaradio/modelo/IRadio.java
@@ -0,0 +1,20 @@
+public interface iRadio{
+
+    //Tiene que iniciar apagada y funciona para cambiar la radio de encendida a apagada
+    public boolean power();
+
+    //Treue es Fm y false AM, cuando se llama a la funcion esta cambia entre true o false
+    public boolean frecuencia();
+
+    //True es para arriba y false es para abajo.
+    public void cambiarEmisora(boolean subirBajar);
+
+    //obtener la emisora actual
+    public double emisoraActual();
+
+    //guardar la emisora en un ArrayList es el cual solo guarda al final de la lista la emisora
+    public void guardarEmisora(double emisora);
+
+    //Se le manda la posicion en la que se encuentra la emisora seleccionada
+    public void cargarEmisoraGuardada(int posicion);
+}
